55938e8c65a346c65ea45c15b7b4f6b26a5f27dd
Paulo Toro <pauloat@gmail.com>
2014-10-10 18:38:08 -0300

    contact page funcional

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      # TODO save data
-      # TODO send message
+      ContactMailer.contact_email(@contact).deliver
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
     else
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ActionMailer::Base
 
   def contact_email(contact)
     @contact = contact
-    mail(to: ENV["OWNER_EMAIL"], from: @contact.email, subject => "Website contact")
+    mail(to: ENV["OWNER_EMAIL"], from: @contact.email, :subject => "Website contact")
   end
 end
